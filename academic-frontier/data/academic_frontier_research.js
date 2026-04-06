811f30e520fd98cb6e3ffc53965de474854e1748
Sichen Tao <sichen.tao@tohoku.ac.jp>
2026-04-06 13:14:46 +0000

    Publish homepage from source (d073b50)

--- academic-frontier/data/academic_frontier_research.js
+++ academic-frontier/data/academic_frontier_research.js
@@ -1,5 +1,5 @@
 window.ACADEMIC_FRONTIER_RESEARCH = {
-  "generatedAt": "2026-04-06 22:11 JST",
+  "generatedAt": "2026-04-06 22:14 JST",
   "seedProfile": {
     "profileName": "Sichen Tao",
     "summary": {
